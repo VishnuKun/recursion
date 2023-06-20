594a7c00e32e178105f3ae2f5e7d5ded8a7915c4
Vishnu <vishnukun954AV@protonmail.com>
2023-06-20 09:39:55 +0530

    Defined methods to get fibonacci series array one using iteration and other using recursion.

---
+++ lib/assignment_one.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+puts 'Assignment One Initialized'
+
+def fibs(num)
+  sequence = [0, 1]
+  sequence << sequence[-1] + sequence[-2] while sequence.length < num
+  sequence
+end
+
+def fibs_rec(num)
+  # base case
+  if num <= 1
+    num
+  else
+    # recursive case
+    fibs_rec(num - 1) + fibs_rec(num - 2)
+  end
+end
+
+num = 8
+fibs = []
+(0..num - 1).each do |i|
+  fibs << fibs_rec(i)
+end
+
+p fibs
